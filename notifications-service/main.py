b14b16de8d5cd3c0a3ce1e65196a1e5bca5ce5d6
nevned <nevenanedeljkovic03@gmail.com>
2026-05-13 15:48:45 +0200

    KafkaZad06

--- notifications-service/main.py
+++ notifications-service/main.py
@@ -3,12 +3,16 @@ from typing import List
 from models import Notification
 from aiokafka import AIOKafkaConsumer
 from contextlib import asynccontextmanager
-import asyncio, json
+import asyncio
+import json
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    
     consumer = AIOKafkaConsumer(
-        "order-confirmed", 
+        "order-confirmed",
+        "product_not_found_events",
+        "out_of_stock_events",
         bootstrap_servers='kafka:9092',
         group_id="notifications-group",
         auto_offset_reset="earliest"
@@ -29,10 +33,27 @@ async def consume(consumer: AIOKafkaConsumer):
     try:
         async for msg in consumer:
             data = json.loads(msg.value.decode('utf-8'))
-            notification = Notification(order_id=data['order_id'], product_id=data['product_id'], message=f"Order {data['order_id']} for product {data['product_id']} has been placed.")
+            topic = msg.topic
+            
+            if topic == "order-confirmed":
+                message = f"Order {data['order_id']} for product {data['product_id']} has been placed."
+                reason = None
+            else:
+                
+                reason = data.get('error_reason', 'Nepoznata greska')
+                message = f"Order {data['order_id']} rejected. Reason: {reason}"
+            
+            notification = Notification(
+                order_id=data['order_id'],
+                product_id=data['product_id'],
+                message=message,
+                error_reason=reason
+            )
             notifications_db.append(notification)
     except asyncio.CancelledError:
         pass
+    except Exception as e:
+        print(f"Error in notification consumer: {e}")
 
 @app.get("/notifications", response_model=List[Notification])
 def get_notifications():
